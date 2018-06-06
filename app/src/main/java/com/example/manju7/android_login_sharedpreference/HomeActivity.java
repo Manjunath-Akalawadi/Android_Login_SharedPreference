15d67529e3c21fb486057c2bba2be300cc2f949a
manju <manjuakalawadi@gmail.com>
2018-06-07 01:12:08 +0530

    final commit

--- app/src/main/java/com/example/manju7/android_login_sharedpreference/HomeActivity.java
+++ app/src/main/java/com/example/manju7/android_login_sharedpreference/HomeActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-
 public class HomeActivity extends Activity {
     Button logout;
     TextView textView;
